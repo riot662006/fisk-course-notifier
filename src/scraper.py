0ce796ff6da5cd1823daf4f1ef3c4c2cdca52aa2
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-07-19 15:04:47 -0700

    Simplify api calls Seems that the token data is not really necessary to get the result (Probably a bug on their side... got to exploit)
    Logic has been simplified cause it only needs `content-type`

--- src/scraper.py
+++ src/scraper.py
@@ -9,7 +9,6 @@ from urllib.parse import urljoin, urlparse, urlencode
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://fisk-ss.colleague.elluciancloud.com/Student/Courses/"
-CSRF_COOKIE_NAME = '.ColleagueSelfServiceAntiforgery'
 
 
 def create_driver(url, cookies={}, headless=False):
@@ -42,59 +41,16 @@ def create_driver(url, cookies={}, headless=False):
 
 
 def throw_bad_response(response: requests.Response):
-    if response.status_code != 200:
-        print(f"Error: Received status code {response.status_code}")
-        print(response.text)
-
+    response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
     return response
 
 
-def get_token_from_home_response(response: requests.Response):
-    soup = BeautifulSoup(response.text, 'html.parser')
-    token_input = soup.find('input', {'name': '__RequestVerificationToken'})
-
-    if token_input:
-        return token_input['value']
-    else:
-        raise ValueError("CSRF token not found in the response HTML.")
-
-
-def get_request_headers(response: requests.Response):
-    csrf = response.cookies.get(CSRF_COOKIE_NAME)
-    token = get_token_from_home_response(response)
-
-    return {
-        '__isguestuser': 'true',
-        '__requestverificationtoken': token,
-        'accept': 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json, charset=UTF-8',
-        'origin': 'https://fisk-ss.colleague.elluciancloud.com',
-        'priority': 'u=1, i',
-        'referer': response.url,
-        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Opera GX";v="119"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36',
-        'x-requested-with': 'XMLHttpRequest',
-        'cookie': f'{CSRF_COOKIE_NAME}={csrf};',
-    }
-
-
-def fetch_request_headers(session: requests.Session, url: str):
-    home_response = throw_bad_response(session.get(url))
-    headers = get_request_headers(home_response)
-
-    return headers
-
-
-def fetch_courses(session, headers, search_criteria):
+def fetch_courses(session, search_criteria):
     response = session.post(
         urljoin(BASE_URL, "PostSearchCriteria"),
-        headers=headers,
+        headers={
+        'content-type': 'application/json, charset=UTF-8',
+    },
         data=json.dumps(search_criteria)
     )
     throw_bad_response(response)
@@ -104,9 +60,8 @@ def fetch_courses(session, headers, search_criteria):
 
 if __name__ == "__main__":
     session = requests.Session()
-    headers = fetch_request_headers(session, BASE_URL)
 
-    courses = fetch_courses(session, headers, search_criteria={"terms": ["2025FA"], "keywordComponents": [
+    courses = fetch_courses(session, search_criteria={"terms": ["2025FA"], "keywordComponents": [
         {"subject": "CSCI", "courseNumber": "", "section": "", "synonym": ""},
         {"subject": "ART", "courseNumber": "", "section": "", "synonym": ""}
     ], })
