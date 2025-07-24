b8f751c5968718fdf52365d88a618eb4173d7114
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-07-24 15:22:50 -0700

    Add caching functions to scraper

--- src/scraper.py
+++ src/scraper.py
@@ -5,9 +5,11 @@ import json
 from urllib.parse import urljoin
 
 from .base_scraper import BASE_URL
+from .course import Course
+from .custom_types import CourseData
 from .section import Section
 
-from .course import Course
+COURSE_DATA_FILE_PATH = "output/course_data.json"
 
 
 def fetch_courses(session: requests.Session, search_criteria: dict[str, Any]):
@@ -45,3 +47,19 @@ def fetch_sections_by_course_labels(session: requests.Session, course_labels: li
         for course in courses
         for section in course.fetch_sections(session)
     ]
+
+
+def load_courses(path: str = COURSE_DATA_FILE_PATH) -> dict[str, CourseData]:
+    try:
+        with open(path, 'r') as f:
+            return json.load(f)
+    except (json.JSONDecodeError):
+        raise ValueError(
+            f"Could not load courses from {path}. File may be missing or corrupted."
+        )
+
+
+def save_courses(courses: list[Course], path: str = COURSE_DATA_FILE_PATH):
+    with open(path, 'w') as f:
+        json.dump({course.get_course_label(): course.to_dict()
+                  for course in courses}, f, indent=4)
--- tests/unit/test_course.py
+++ tests/unit/test_course.py
@@ -1,12 +1,13 @@
+from src.scraper import load_courses, save_courses
 from src.course import Course
 
 ex_course = Course({
-        "Id": 101,
-        "MatchingSectionIds": ["1234", "5678"],
-        "Title": "Intro to Computing",
-        "SubjectCode": "CSCI",
-        "Number": "101"
-    })
+    "Id": 101,
+    "MatchingSectionIds": ["1234", "5678"],
+    "Title": "Intro to Computing",
+    "SubjectCode": "CSCI",
+    "Number": "101"
+})
 
 
 def test_course_to_dict():
@@ -17,6 +18,42 @@ def test_course_to_dict():
     assert result["subjectCode"] == "CSCI"
     assert result["subjectNumber"] == "101"
 
+
 def test_get_course_label():
     label = ex_course.get_course_label()
     assert label == "CSCI-101"
+
+
+def test_save_n_load_courses():
+    path = 'output/tmp'
+    save_courses([ex_course], path)
+
+    courses = list(load_courses(path).values())
+
+    assert courses == [
+        ex_course.to_dict()], "Saved course data does not match expected output."
+
+
+def test_save_n_load_courses_doesnt_exist():
+    path = 'output/asdfghjkzxcvbnwertyu1234567890'
+
+    try:
+        load_courses(path)
+        raise RuntimeError(
+            "Expected an error when trying to save to a non-existent path, but none was raised."
+        )
+    except FileNotFoundError:
+        pass
+
+    path = 'output/tmp'
+    with open(path, 'w') as f:
+        f.write("This is not a valid JSON file.")
+
+    try:
+        load_courses(path)
+        raise RuntimeError(
+            "Expected an error when trying to load a corrupted JSON file, but none was raised."
+        )
+    except ValueError as e:
+        assert str(
+            e) == f"Could not load courses from {path}. File may be missing or corrupted."
