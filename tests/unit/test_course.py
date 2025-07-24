016468e679b9d4dca4244d6deccc26eb2c49a79f
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-07-24 14:12:15 -0700

    Set example course for test

--- tests/unit/test_course.py
+++ tests/unit/test_course.py
@@ -1,8 +1,6 @@
 from src.course import Course
 
-
-def test_course_to_dict():
-    course = Course({
+ex_course = Course({
         "Id": 101,
         "MatchingSectionIds": ["1234", "5678"],
         "Title": "Intro to Computing",
@@ -10,9 +8,15 @@ def test_course_to_dict():
         "Number": "101"
     })
 
-    result = course.to_dict()
+
+def test_course_to_dict():
+    result = ex_course.to_dict()
     assert result["courseId"] == 101
     assert result["sectionIds"] == ["1234", "5678"]
     assert result["title"] == "Intro to Computing"
     assert result["subjectCode"] == "CSCI"
     assert result["subjectNumber"] == "101"
+
+def test_get_course_label():
+    label = ex_course.get_course_label()
+    assert label == "CSCI-101"
